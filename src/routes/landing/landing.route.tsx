f1c53e9d48f158042d192432cf697dc081f5380b
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-12-03 08:52:55 -0500

    completed landing page

--- src/routes/landing/landing.route.tsx
+++ src/routes/landing/landing.route.tsx
@@ -1,3 +1,29 @@
-export function LandingPage(){
-    return<h1>Landing Page</h1>
+import { Box, Button, Container, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { LayoutContainer } from "../../components/layout/layout.component";
+
+export function LandingPage() {
+  return (
+    <>
+      <Typography variant="h1">React TicTac Toe</Typography>
+      <Typography variant="h3">Hello everyone!</Typography>
+      <Typography variant="body1">
+        This week I decided to make a to do list app using typescript!
+      </Typography>
+      <Typography variant="body1">
+        It was a lot of fun to make and I learned a lot about typescript and how
+        to use it.
+      </Typography>
+      <Typography variant="body1">
+        It was also a lot of fun to make a to do list app because I use to do
+        list apps all the time.
+      </Typography>
+      <Typography variant="body1">
+        Wanna check it out? Click on the button below
+      </Typography>
+      <Button variant="contained" component={Link} to="/app" >
+        <Typography variant="button">My App</Typography>
+      </Button>
+    </>
+  );
 }
