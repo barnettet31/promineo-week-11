75cd96623b5fc5ec522849e30c4028d108e2d18b
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-12-06 22:26:05 -0500

    added hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { MuiThemeProvider } from "./infrastructure/theme.provider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <MuiThemeProvider>
         <App />
       </MuiThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
