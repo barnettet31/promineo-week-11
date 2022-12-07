a5bd9ae424f347f82e04ee7f88a429d7197fc27c
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-12-06 22:15:00 -0500

    made changes to the router

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter, Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { MuiThemeProvider } from "./infrastructure/theme.provider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <MuiThemeProvider>
         <App />
       </MuiThemeProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
